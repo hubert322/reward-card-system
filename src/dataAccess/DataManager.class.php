e7ec3b04a859da390418c60d936312bf59e709c4
hubert322 <hhung@HHUNG-201704.clg.local>
2019-07-31 11:04:39 -0400

    Finished setting up data manager and import into reward card service

--- src/dataAccess/DataManager.class.php
+++ src/dataAccess/DataManager.class.php
@@ -3,6 +3,9 @@ declare (strict_types=1);
 
 namespace dataAccess;
 
+use mysqli;
+use Exception;
+
 class DataManager
 {
     const DB_ACCOUNTS = "accounts";
@@ -10,17 +13,52 @@ class DataManager
 
     const LOC_MASTER = "master";
 
-    private const SERVER_NAME = "159.89.86.178";
-    private const USERNAME = "root";
-    private const PASSWORD = "zcadqe13";
-
-    private $database;
-    private $location;
-    private $shardConfigurationId;
     private $conn;
+    private $result;
+    private $lastInsertedId;
 
     public function __construct(string $database, string $location, int $shardConfigurationId = null)
     {
-        $this->conn = new mysqli (self::SERVER_NAME, self::USERNAME, self::PASSWORD);
+        if (!$this->parametersValid ($database, $location))
+        {
+            throw new Exception ("Invalid parameters for DataManager");
+        }
+
+        $this->conn = new mysqli ($_ENV["SERVER_NAME"], $_ENV["USERNAME"], $_ENV["PASSWORD"]);
+        if ($this->conn->connect_error)
+        {
+            die ("Connection failed: " . $this->conn->connect_error);
+        }
+        $this->conn->select_db ($database);
+    }
+
+    public function __destruct()
+    {
+        $this->conn->close ();
+    }
+
+    public function query (string $sql): void
+    {
+        $this->result = $this->conn->query ($sql);
+        $this->lastInsertedId = $this->conn->insert_id;
+    }
+
+    public function fetch (): array
+    {
+        return $this->result->fetch_assoc ();
+    }
+
+    public function lastId (): int
+    {
+        return $this->lastInsertedId;
+    }
+
+    private function parametersValid (string $database, string $location)
+    {
+        if ($database !== self::DB_ACCOUNTS && $database !== self::DB_RK_ACTIVITY)
+            return false;
+        if ($location !== self::LOC_MASTER)
+            return false;
+        return true;
     }
 }
