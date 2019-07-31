5d9ac157af5e7070d48e843eda1306ff2fb0b782
hubert322 <hhung@HHUNG-201704.clg.local>
2019-07-31 11:59:05 -0400

    added qr code library and include all necessary files in index.php

--- index.php
+++ index.php
@@ -1,15 +1,25 @@
 <?php
 
-$file = fopen ("./env.conf", "r");
-while (!feof ($file))
+$file = @fopen ("./env.conf", "r");
+if ($file)
 {
-    $line = fgets ($file);
-    if ($line)
+    while (!feof ($file))
     {
-        [0 => $key, 1 => $value] = explode (": ", $line);
-        $_ENV[$key] = $value;
+        $line = fgets ($file);
+        if ($line)
+        {
+            [0 => $key, 1 => $value] = explode (": ", $line);
+            $_ENV[$key] = $value;
+        }
     }
+    fclose ($file);
 }
-fclose ($file);
 
+require_once "./src/dataAccess/DataManager.class.php";
+require_once "./src/dataAccess/RewardCardDbGateway.class.php";
+require_once "./src/services/QrCodeService.class.php";
+require_once "./src/services/RewardCardService.class.php";
+require_once "./src/services/SessionInfoCacheService.class.php";
+require_once "./src/api/RewardCardApiController.class.php";
+require_once "./src/api/Router.php";
 require_once "./index.html";
