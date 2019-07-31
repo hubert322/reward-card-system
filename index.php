bd038a8faca279f70bb5f37fd3d58b4ecbdcc1b9
hubert322 <hhung@HHUNG-201704.clg.local>
2019-07-31 16:14:49 -0400

    Moved setting up environment variables to api.php and added SQLUtil in include

--- index.php
+++ index.php
@@ -1,23 +1,5 @@
 <?php
 
-// Setup Environment variables
-$file = @fopen ("./env.conf", "r");
-if ($file)
-{
-    while (!feof ($file))
-    {
-        $line = fgets ($file);
-        if ($line)
-        {
-            [0 => $key, 1 => $value] = explode (": ", $line);
-            $_ENV[$key] = $value;
-        }
-    }
-    fclose ($file);
-}
-
-session_start ();
-
 require_once "./index.html";
 
 
