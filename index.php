9dbcedf6b1c32b2985a36a1d54cf73d04fe0169b
hubert322 <hhung@HHUNG-201704.clg.local>
2019-07-31 10:29:54 -0400

    Prevent from reading empty line and added jquery

--- index.php
+++ index.php
@@ -3,8 +3,12 @@
 $file = fopen ("./env.conf", "r");
 while (!feof ($file))
 {
-    [0 => $key, 1 => $value] = explode (": ", fgets ($file));
-    $_ENV[$key] = $value;
+    $line = fgets ($file);
+    if ($line !== "\n")
+    {
+        [0 => $key, 1 => $value] = explode (": ", $line);
+        $_ENV[$key] = $value;
+    }
 }
 
 foreach ($_ENV as $k => $v)
