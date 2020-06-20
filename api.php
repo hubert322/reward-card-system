4776f69828e8f13cc890b78b0bf83587981bbc16
hubert322 <huberth@umich.edu>
2020-06-20 13:06:10 +0800

    fixing download filename

--- api.php
+++ api.php
@@ -1,8 +1,6 @@
 <?php
 
 header("Access-Control-Allow-Origin: *");
-// header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-// header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 session_start ();
 
 require_once dirname($_SERVER["DOCUMENT_ROOT"]) . "/vendor/autoload.php";
